8dca9fcc97b21a37b148a30843d84254da65c855
BertrandPeltier <bertrandpeltier1979@gmail.com>
2021-07-29 23:23:32 +0200

    <a> preventDefault : done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-console.log('Test');
+const app = {
+    init: () => {
+        document.querySelectorAll('a').forEach(anchor => {
+            anchor.addEventListener('click', (event) => {
+                event.preventDefault();
+            })
+        })
+    }
+};
+
+document.addEventListener('DOMContentLoaded', app.init);
